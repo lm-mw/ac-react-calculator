11ed3cd21fa7e69dcaa54ddde1bdd159358b9a8c
lm-mw <logan.m.wright1@gmail.com>
2021-04-20 13:35:43 -0600

    test

---
+++ build/precache-manifest.6be023d2fedd6d5336278bbdeccf1f80.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "6af55404a66fb7cb32ac",
+    "url": "/ReactCalculator/static/css/main.fcbfd819.chunk.css"
+  },
+  {
+    "revision": "6af55404a66fb7cb32ac",
+    "url": "/ReactCalculator/static/js/main.6af55404.chunk.js"
+  },
+  {
+    "revision": "b5331b3206fcecb5da6b",
+    "url": "/ReactCalculator/static/css/1.5547dd13.chunk.css"
+  },
+  {
+    "revision": "b5331b3206fcecb5da6b",
+    "url": "/ReactCalculator/static/js/1.b5331b32.chunk.js"
+  },
+  {
+    "revision": "855df1d8b3b172117c20",
+    "url": "/ReactCalculator/static/js/runtime~main.855df1d8.js"
+  },
+  {
+    "revision": "40de76850a11ffa42d24f90739259c8a",
+    "url": "/ReactCalculator/static/media/menu.40de7685.svg"
+  },
+  {
+    "revision": "6716dd950cb0425751afd0890d5e26f5",
+    "url": "/ReactCalculator/static/media/nunito-latin-200.6716dd95.woff2"
+  },
+  {
+    "revision": "06dea2b4088a104f4ace408e07b51d2e",
+    "url": "/ReactCalculator/static/media/nunito-latin-200italic.06dea2b4.woff2"
+  },
+  {
+    "revision": "4b3dd3e005c2850f7299928bf3a96420",
+    "url": "/ReactCalculator/static/media/nunito-latin-200.4b3dd3e0.woff"
+  },
+  {
+    "revision": "6d3fe135f7e6af19b321b1af6f5604ff",
+    "url": "/ReactCalculator/static/media/nunito-latin-300italic.6d3fe135.woff2"
+  },
+  {
+    "revision": "fe67e10445323683dab69663f6a4718a",
+    "url": "/ReactCalculator/static/media/nunito-latin-400.fe67e104.woff2"
+  },
+  {
+    "revision": "3eb1a100f527e8139eafee00d6ce77db",
+    "url": "/ReactCalculator/static/media/nunito-latin-300.3eb1a100.woff"
+  },
+  {
+    "revision": "f708870c910aac39d7c23b67e6dff69a",
+    "url": "/ReactCalculator/static/media/nunito-latin-300.f708870c.woff2"
+  },
+  {
+    "revision": "429b4b7f49b28a90ca3abe415f9319f9",
+    "url": "/ReactCalculator/static/media/nunito-latin-200italic.429b4b7f.woff"
+  },
+  {
+    "revision": "f631a08dead09cf6e99c3f5ca9d8de09",
+    "url": "/ReactCalculator/static/media/nunito-latin-400italic.f631a08d.woff2"
+  },
+  {
+    "revision": "2c14852159c34ea2b6813c03474fdd24",
+    "url": "/ReactCalculator/static/media/nunito-latin-300italic.2c148521.woff"
+  },
+  {
+    "revision": "feb87cc98b5b4c5c706bd5745e1e3b14",
+    "url": "/ReactCalculator/static/media/nunito-latin-400.feb87cc9.woff"
+  },
+  {
+    "revision": "e57dde6f29f1255d69b2a7daf9bde8d9",
+    "url": "/ReactCalculator/static/media/nunito-latin-600.e57dde6f.woff2"
+  },
+  {
+    "revision": "aafaf2120bb63f267700c50a78784bf9",
+    "url": "/ReactCalculator/static/media/nunito-latin-400italic.aafaf212.woff"
+  },
+  {
+    "revision": "ae6d08e9151bfae03736df358d0ab384",
+    "url": "/ReactCalculator/static/media/nunito-latin-600.ae6d08e9.woff"
+  },
+  {
+    "revision": "0fdd3929797075a61d4c79d02b37270b",
+    "url": "/ReactCalculator/static/media/nunito-latin-600italic.0fdd3929.woff2"
+  },
+  {
+    "revision": "dd42f32ac2e37e4d8705820d0b8dd202",
+    "url": "/ReactCalculator/static/media/nunito-latin-700.dd42f32a.woff2"
+  },
+  {
+    "revision": "086733827e6d24ba33e7301e9fda2c49",
+    "url": "/ReactCalculator/static/media/nunito-latin-700.08673382.woff"
+  },
+  {
+    "revision": "3a0274f57ae6a6e1954bb2975ce8f7c2",
+    "url": "/ReactCalculator/static/media/nunito-latin-700italic.3a0274f5.woff2"
+  },
+  {
+    "revision": "7ddb3a649cb1a3bb8831d1427a2a0590",
+    "url": "/ReactCalculator/static/media/nunito-latin-600italic.7ddb3a64.woff"
+  },
+  {
+    "revision": "389848e315f1e04362c57cb4432bfd3a",
+    "url": "/ReactCalculator/static/media/nunito-latin-800.389848e3.woff2"
+  },
+  {
+    "revision": "812a8ec82e815eaab23ddc0ee1cdc641",
+    "url": "/ReactCalculator/static/media/nunito-latin-700italic.812a8ec8.woff"
+  },
+  {
+    "revision": "e345e7e8dd10a08f0d77e083aca4aea2",
+    "url": "/ReactCalculator/static/media/nunito-latin-800.e345e7e8.woff"
+  },
+  {
+    "revision": "26ad2c5e83ffb419728b2f3621eb1314",
+    "url": "/ReactCalculator/static/media/nunito-latin-800italic.26ad2c5e.woff2"
+  },
+  {
+    "revision": "f5ebcebbcce3cc4141e85f483bd957da",
+    "url": "/ReactCalculator/static/media/nunito-latin-800italic.f5ebcebb.woff"
+  },
+  {
+    "revision": "6d3158a3e4568c98aafa029b35d0937a",
+    "url": "/ReactCalculator/static/media/nunito-latin-900.6d3158a3.woff2"
+  },
+  {
+    "revision": "f3daf4bfc8f38eddf982522a227fb80b",
+    "url": "/ReactCalculator/static/media/nunito-latin-900italic.f3daf4bf.woff2"
+  },
+  {
+    "revision": "b1b0d941775ac87980e5542551603dfb",
+    "url": "/ReactCalculator/static/media/nunito-latin-900.b1b0d941.woff"
+  },
+  {
+    "revision": "2ebb1fcb479acc2f6fc58d9a89295bdf",
+    "url": "/ReactCalculator/static/media/nunito-latin-900italic.2ebb1fcb.woff"
+  },
+  {
+    "revision": "d57522a412487deb1c204c88c5b2b9bd",
+    "url": "/ReactCalculator/index.html"
+  }
+];
